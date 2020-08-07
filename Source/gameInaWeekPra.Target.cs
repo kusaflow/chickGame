e7cbfbeac935f049e78b274bf243b7ba6fc18972
Kunal SAgar <kunalsagar1999@gmail.com>
2020-08-08 00:42:00 +0530

    final

--- Source/gameInaWeekPra.Target.cs
+++ Source/gameInaWeekPra.Target.cs
@@ -10,5 +10,12 @@ public class gameInaWeekPraTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "gameInaWeekPra" } );
+
+if (Target.Platform == UnrealTargetPlatform.Android)
+    {
+        ExtraModuleNames.Add("OnlineSubsystemGooglePlay");
+        ExtraModuleNames.Add("OnlineSubsystem");
+        ExtraModuleNames.Add("AndroidAdvertising");
+    }
 	}
 }
